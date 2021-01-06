6bd8dc2504db932cbf771433de396d1d87cf6ca3
jzrlza <parinvut12345@gmail.com>
2021-01-06 22:44:10 +0700

    Moved the Moving Average code to its own function.

--- sound_file_slience_detector.py
+++ sound_file_slience_detector.py
@@ -3,6 +3,31 @@ import numpy as np
 import wave
 import sys
 
+#Moving average for smoother curves
+#if moving_averages = 1, then the output is the same dataset as the input
+#the more, the smoother the output dataset
+#input_data and output_data (return) are both numpy datasets
+def moving_average(moving_averages, input_data):
+    output_data = np.array([])
+    for j in range(len(input_data) - 1) :
+        print("%d of %d" % (j, len(input_data)))
+        x = 0.0
+        print(f"%d to %d" % (j,j+moving_averages-1))
+        for i in range(j,j+moving_averages):
+            x_before = x
+            x += input_data[i]
+            print(f"%d : (%.2f)+(%.2f) = %.2f" % (i, x_before, input_data[i], x))
+
+        print(f"Summed : %.2f" % (x))
+        x = float(x) / float(moving_averages)
+        print(f"Averaged : %.2f" % (x))
+
+        output_data = np.append(output_data, x)
+        print("Result")
+        print(output_data)
+
+    return output_data
+
 #path of the audio file
 audio_data = wave.open("test_voice.wav", "r")
 
@@ -35,23 +60,7 @@ print("Timesteps per sec : "+str(time_steps_per_second))
 #the more, the smoother
 moving_averages = 3
 
-smooth_signal = np.array([]) 
-
-for j in range(signal_steps - 1) :
-    x = 0.0
-    print(f"%d to %d" % (j,j+moving_averages-1))
-    for i in range(j,j+moving_averages):
-        x_before = x
-        x += signal[i]
-        print(f"%d : (%.2f)+(%.2f) = %.2f" % (i, x_before, signal[i], x))
-
-    print(f"Summed : %.2f" % (x))
-    x = float(x) / float(moving_averages)
-    print(f"Averaged : %.2f" % (x))
-
-    smooth_signal = np.append(smooth_signal, x)
-    print("Result")
-    print(smooth_signal)
+smooth_signal = moving_average(moving_averages, signal)
 
 print("Finished")
 
