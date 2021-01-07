bff096a1b89970a6b5b09e3721ae07a3bc15a6ef
jzrlza <parinvut12345@gmail.com>
2021-01-07 21:39:40 +0700

    Timestamp output implemented.

--- sound_silence_detector.py
+++ sound_silence_detector.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import wave
 import sys
 import math
+import json
 
 #####Functions#####
 
@@ -42,7 +43,7 @@ if audio_data.getnchannels() == 2:
 time = np.linspace(0, len(signal) / fs, num=len(signal))
 time = time.tolist()
 ##Threshold settings##
-voice_sd_threshold = 160
+voice_sd_threshold = 320
 steps_threshold = 20
 
 #Values
