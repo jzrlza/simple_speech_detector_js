c22a6fb5c442b19d98ee813069f15e81dfad6abd
jzrlza <parinvut12345@gmail.com>
2021-01-07 21:33:20 +0700

    Averaged right.

--- sound_silence_detector.py
+++ sound_silence_detector.py
@@ -5,12 +5,20 @@ import sys
 import math
 
 #####Functions#####
-def ema(data, summation, n):
+
+def sma(data, t):
+    print("dd")
+
+def ema(data, t, i):
+    if i == 0:
+        return sma(data,t, start, end)
+    k = 2/(t+1)
+    ema = data[i]*k + (1-k)*data[i-1]
+    return ema
     #n = end_point + 1 - start_point
     #for timestep in range(start_point, end_point+1):
     #    print("dd")
     #    if timestep == 0:
-    print("Dd")
 
 def get_sd(data, mean, start, end):
     summation = 0.0
@@ -34,8 +42,8 @@ if audio_data.getnchannels() == 2:
 time = np.linspace(0, len(signal) / fs, num=len(signal))
 time = time.tolist()
 ##Threshold settings##
-voice_sd_threshold = 5
-steps_threshold = 12
+voice_sd_threshold = 160
+steps_threshold = 20
 
 #Values
 timestamp_of_recent_silence = {}
@@ -55,7 +63,7 @@ for timestamp in range(len(signal)):
         #print("SUM "+str(average_chunk))
         average_chunk = summation / steps_threshold
         #print("AVG "+str(average_chunk))
-        
+        #mean = ema(data, t, i)
 
         sd = get_sd(signal,average_chunk,start,timestamp)
         #print("SD "+str(sd))
@@ -70,11 +78,15 @@ for timestamp in range(len(signal)):
         prev_sd = sd
         sd = 0.0
         
+###OUTPUT###
+with open("output_timestamps.json","w", encoding="utf-8") as jsonfile:
+    json.dump(timestamp_of_recent_silence, jsonfile, ensure_ascii=False, indent=2)
 
+##VISUALIZE##
 plt.figure(1)
 plt.title("Signal Wave...")
 
-marks = [time.index(i) for i in timestamp_of_recent_silence_lst]
-
-plt.plot(time, signal, markevery=marks, ls="", marker="o", label="points")
+plt.plot(time, signal, label="sound")
+plt.plot(time, signal, markevery=timestamp_of_recent_silence_lst, ls="", marker="o", label="points")
 plt.show()
+
